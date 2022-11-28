932c9d8a77568cb5c7d7a6d240f110cb2b5388dc
Mário Rodrigues <mjsrodrigues4@gmail.com>
2022-11-28 23:05:02 +0000

    add [docker] config

---
+++ docker/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#set -Eeuo pipefail
+
+BASE_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+cd "${BASE_DIR}/.." || exit 127
+
+# shellcheck source=../scripts/extras.sh
+. scripts/extras.sh
+# shellcheck source=../scripts/utils.sh
+. scripts/utils.sh
+
+ask_for_sudo
+
+install_package docker
+
+install_package docker-compose
+
+execute "sudo usermod -aG docker ${USER}" "Add docker to usergroup"
